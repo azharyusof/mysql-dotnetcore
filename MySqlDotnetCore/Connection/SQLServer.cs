4b7f00f47346b4bcd9d02706b682b29cb03b2e66
azhar.yusof <azhar.yusof@edgenta.com>
2021-12-04 10:38:30 +0800

    Update SQLServer.cs

--- MySqlDotnetCore/Connection/SQLServer.cs
+++ MySqlDotnetCore/Connection/SQLServer.cs
@@ -11,7 +11,6 @@ namespace MySqlDotnetCore.Connection
         {
             var connection = new Models.Connection();
 
-            //connection.connectionstring = "Data Source= 10.49.45.40; Database=BLMS; User ID = Appsa; Password=Opuswebsql2018; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             connection.connectionstring = "Data Source = 10.249.1.125; Database=test;User ID = Appsa; Password=Opuswebsql2017;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             return connection;
